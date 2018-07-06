d0fa11ece8f63c52a61361ec0f08270a191e85dd
Daniel Alexander Vasilyonok <danielv@bu.edu>
2018-07-06 21:03:07 +0000

    Database Model and Location Data Import

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, render_template
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -26,9 +26,35 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
+    """ Home Page """
+
     KEY = "8565f94778670d37d36772bcc5b2a904"
     get_request = f"https://api.darksky.net/forecast/{KEY}/42.37,-71.11"
     weather = requests.get(get_request).json()
     summary = weather["currently"]["summary"]
     #print(json.dumps(weather["currently"], indent = 2))
     return render_template("home.html", summary=summary)
+
+@app.route("/search", methods=["POST"])
+def login_register():
+    """ Login or Register """
+
+    user_name = request.form.get("username")
+    pass_word = request.form.get("password")
+
+    if request.form["submit"] == "Login":
+
+        # Check if username exists in DB. If it doesn't alert user that they must Register
+
+        # If username exists and password correct
+        return render_template("search.html", user_name=user_name)
+
+        # If username exists but password wrong, alert user they must re-enter password
+
+    elif request.form["submit"] == "Register":
+
+        # Make sure username is not in DB. If username is in DB, alert user they must Login. Not Register.
+
+        # If username not in DB, Allow user to register and take them to search page
+
+        return render_template("search.html", user_name=user_name)
--- import.py
+++ import.py
@@ -1,11 +1,54 @@
 from pandas import read_csv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+import os
 
-def read_zips():
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+def read_data():
     return read_csv("zips.csv")
 
 def main():
-    zips_df = read_zips()
-    print(zips_df.head(10))
+    data_df = read_data()
+
+    population_zip_df = data_df[["Zipcode", "Population"]]
+    population_zip_df.columns = ["zip", "population"]
+    locations_df = data_df[["City", "State", "Lat", "Long"]]
+    locations_df.columns = ["city", "state", "lat", "long"]
+
+    '''
+    to_sql not working
+    population_zip_df.to_sql(name="population_zip", con=engine, index=False, index_label="id", if_exists="append")
+    locations_df.to_sql(name="locations", con=engine, index=False, index_label="id", if_exists="append")
+    '''
+
+    for row in population_zip_df.iterrows():
+        zip_code = row[1][0]
+        population = row[1][1]
+
+        db.execute("INSERT INTO population_zip (zip, population) VALUES (:zip_code, :population)",
+                    {"zip_code": int(zip_code), "population": int(population)})
+        print(f"Added unique (zip code, population) pair with zip code: {zip_code} and population: {population}")
+
+    db.commit()
+
+    for row in locations_df.iterrows():
+        city = row[1][0]
+        state = row[1][1]
+        lat = row[1][2]
+        longg = row[1][3]
+
+        db.execute("INSERT INTO locations (city, state, lat, long) VALUES (:city, :state, :lat, :longg)",
+                    {"city": city, "state": state, "lat": float(lat), "longg": float(longg)})
+        print(f"Added location: city: {city} state: {state} lat: {lat} long: {longg}")
+
+    db.commit()
 
 if __name__ == "__main__":
     main()
