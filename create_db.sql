190317e6a1b1e2c8c931efc87d549fe3892360c6
Daniel Alexander Vasilyonok <danielv@bu.edu>
2018-07-07 16:16:41 +0000

    CSV to DB Import

--- create_db.sql
+++ create_db.sql
@@ -6,39 +6,30 @@ CREATE TABLE Users (
     password VARCHAR NOT NULL
 );
 
-/* Population and Zip codes
-   This table has every unique location.
-*/
+
 CREATE TABLE Population_Zip (
     id SERIAL PRIMARY KEY,
     zip INTEGER NOT NULL,
-    population INTEGER NOT NULL
+    population INTEGER NOT NULL,
+    location_id INTEGER REFERENCES Locations
 );
 
-/*
-Locations
-There is a 1-to-Many relationship between each location and each (Zip code, Population) Pair
-*/
+
 CREATE TABLE Locations (
     id SERIAL PRIMARY KEY,
     city VARCHAR NOT NULL,
     state VARCHAR NOT NULL,
     lat DECIMAL NOT NULL,
     long DECIMAL NOT NULL,
-    location_id INTEGER REFERENCES Population_Zip
 );
 
-/* Keep track of Check in count for each unique zip code*/
 CREATE TABLE CheckIn (
     id SERIAL PRIMARY KEY,
     check_in_count INTEGER DEFAULT 0,
     location_id INTEGER REFERENCES Population_Zip
 );
 
-/*
-Comments
-There is a Many-to-1 relationship between comments and users.
-*/
+
 CREATE TABLE Comments (
     id SERIAL PRIMARY KEY,
     comment VARCHAR NOT NULL,
--- import.py
+++ import.py
@@ -14,41 +14,71 @@ db = scoped_session(sessionmaker(bind=engine))
 def read_data():
     return read_csv("zips.csv")
 
+def create_tables():
+
+    # Create Users Table
+    db.execute('CREATE TABLE users ('
+               'id SERIAL PRIMARY KEY,'
+               'username VARCHAR NOT NULL,'
+               'pw_hash VARCHAR NOT NULL);')
+
+    # Create Comments Table
+    db.execute('CREATE TABLE Comments ('
+               'id SERIAL PRIMARY KEY,'
+               'comment VARCHAR NOT NULL,'
+               'user_id INTEGER REFERENCES Users(id));')
+
+    # Create Locations
+    db.execute('CREATE TABLE Locations ('
+            'id SERIAL PRIMARY KEY,'
+            'Zipcode VARCHAR NOT NULL,'
+            'City VARCHAR NOT NULL,'
+            'State VARCHAR NOT NULL,'
+            'Lat DECIMAL NOT NULL,'
+            'Long DECIMAL NOT NULL,'
+            'Population INTEGER NOT NULL);')
+
+    # Create Checkins
+    db.execute('CREATE TABLE Checkin ('
+            'id SERIAL PRIMARY KEY,'
+            'check_in_count INTEGER DEFAULT 0,'
+            'location_id INTEGER REFERENCES Locations(id));')
+
+    db.commit()
+
 def main():
+
+    # Create DB Tables
+    create_tables()
+
+    # Read Location Data
     data_df = read_data()
 
-    population_zip_df = data_df[["Zipcode", "Population"]]
-    population_zip_df.columns = ["zip", "population"]
-    locations_df = data_df[["City", "State", "Lat", "Long"]]
-    locations_df.columns = ["city", "state", "lat", "long"]
+    # Cast Zipcode to string and add leading zeros to zip codes of length 4
+    data_df["Zipcode"] = data_df["Zipcode"].astype(str)
+    mask = (data_df["Zipcode"].str.len() == 4)
+    data_df.loc[mask, "Zipcode"] = data_df.loc[mask, "Zipcode"].apply(lambda zip_code: f"0{zip_code}")
+
+    # Insert each row from zips.csv into Locations table
+    for row in data_df.iterrows():
+        zip_code = row[1][0]
+        city = row[1][1]
+        state = row[1][2]
+        lat = row[1][3]
+        longg = row[1][4]
+        population = row[1][5]
+
+        db.execute("INSERT INTO Locations (Zipcode, City, State, Lat, Long, Population) VALUES (:zip_code, :city, :state, :lat, :long, :population)",
+                  {"zip_code": zip_code, "city": city, "state": state, "lat": float(lat), "long": float(longg), "population": population})
+        print(f"Added location with Zipcode: {zip_code}, City: {city}, State: {state}, lat: {lat}, long: {longg}, population: {population}")
+
+    db.commit()
 
     '''
     to_sql not working
     population_zip_df.to_sql(name="population_zip", con=engine, index=False, index_label="id", if_exists="append")
     locations_df.to_sql(name="locations", con=engine, index=False, index_label="id", if_exists="append")
     '''
-
-    for row in population_zip_df.iterrows():
-        zip_code = row[1][0]
-        population = row[1][1]
-
-        db.execute("INSERT INTO population_zip (zip, population) VALUES (:zip_code, :population)",
-                    {"zip_code": int(zip_code), "population": int(population)})
-        print(f"Added unique (zip code, population) pair with zip code: {zip_code} and population: {population}")
-
-    db.commit()
-
-    for row in locations_df.iterrows():
-        city = row[1][0]
-        state = row[1][1]
-        lat = row[1][2]
-        longg = row[1][3]
-
-        db.execute("INSERT INTO locations (city, state, lat, long) VALUES (:city, :state, :lat, :longg)",
-                    {"city": city, "state": state, "lat": float(lat), "longg": float(longg)})
-        print(f"Added location: city: {city} state: {state} lat: {lat} long: {longg}")
-
-    db.commit()
 
 if __name__ == "__main__":
     main()
